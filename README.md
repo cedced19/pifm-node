f649278031420de49dfc3e3875f7c65dd0f81cef
Cédric <cedced19@gmail.com>
2015-02-05 18:27:44 +0100

    Initilal commit

---
+++ README.md
@@ -0,0 +1,38 @@
+#pifm-node
+ 
+A Node.js software to play musics on a __FM radio__ in command line.  
+Only on Raspberry Pi.
+
+[![Build Status](https://travis-ci.org/cedced19/pifm-node.svg)](https://travis-ci.org/cedced19/pifm-node)
+
+![](https://raw.githubusercontent.com/cedced19/pifm-node/master/demo.png)
+
+##What say laws ?
+You do not have the right to broadcast radio in most countries.  
+I am __not__ at all responsible for your actions.  
+
+##Installation
+
+```bash
+$ git clone --depth=1 --branch=master https://github.com/cedced19/pifm-node 
+$ cd ./pifm-node/dist/
+$ npm install
+$ chmod 777 pifm
+$ node pifm-node.js
+```
+
+You can place your musics anywhere in the folder.
+
+
+##Configuration
+
+You can change the frequency, rate or port on `config.json`
+
+See default options:
+
+```json
+{
+    "freq": 108.5,
+    "rate": 22050
+}
+```
---
+++ pifm-node.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict';
+var colors = require('colors'), 
+    config = require('./config.json'),
+    list = require('./lib/ls')('./'), 
+    time = require('./lib/time'),
+    exec = require('child_process').execFile,
+    proc = null,
+    lastzik = false,
+    currentzik = false;
+
+var set = function (zik) {
+    lastzik = currentzik;
+    currentzik = zik; 
+    if (proc) proc.kill();
+    console.log(colors.cyan('['+ time() + ']') + ' Music ' + colors.green(zik.name) + ' is launched!');
+    proc = exec('./pifm', [zik.uri, config.freq, config.rate], null, function() {
+        proc = null;
+        random();
+    });
+};
+
+var random = function () {
+    var number = Math.floor(Math.random() * list.length);
+    if (currentzik != list[number]) {
+        set(list[number]);
+    } else {
+        random();
+    }
+};
+
+console.log(colors.cyan('['+ time() + ']') + ' Now this will broadcast on ' +  colors.green(config.freq) + ' MHz and the rate is ' + colors.green(config.rate) + ' Hz.');
+random();
