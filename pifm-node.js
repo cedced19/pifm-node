30d9a92365322f4e4974cce1eff1d21c279af31d
Cédric <cedced19@gmail.com>
2015-02-24 19:07:29 +0100

    Adding check-update-github

--- pifm-node.js
+++ pifm-node.js
@@ -2,42 +2,57 @@
 'use strict';
 var colors = require('colors'), 
     config = require('./config.json'),
-    list = require('./lib/ls')('./'), 
+    list = require('./lib/ls')('./'),
     time = require('./lib/time'),
     exec = require('child_process').execFile,
-    proc = null,
-    lastzik = false,
+    proc = null, 
+    lastzik = false, 
     currentzik = false;
 
 var set = function (zik) {
-    lastzik = currentzik;
-    currentzik = zik; 
-    if (proc) proc.kill();
-    console.log(colors.cyan('['+ time() + ']') + ' Music ' + colors.green(zik.name) + ' is launched!');
-    proc = exec('./pifm', [zik.uri, config.freq, config.rate], null, function() {
-        proc = null;
-        random();
-    });
+  lastzik = currentzik;
+  currentzik = zik;
+  if (proc)
+    proc.kill();
+  console.log(colors.cyan('[' + time() + ']') + ' Music ' + colors.green(zik.name) + ' is launched!');
+  proc = exec('./pifm', [
+    zik.uri,
+    config.freq,
+    config.rate
+  ], null, function () {
+    proc = null;
+    random();
+  });
 };
 
 var random = function () {
-    var number = Math.floor(Math.random() * list.length);
-    if (currentzik != list[number]) {
-        set(list[number]);
-    } else {
-        random();
-    }
+  var number = Math.floor(Math.random() * list.length);
+  if (currentzik != list[number]) {
+    set(list[number]);
+  } else {
+    random();
+  }
 };
 
 var start = function () {
-    console.log(colors.cyan('['+ time() + ']') + ' Now this will broadcast on ' +  colors.green(config.freq) + ' MHz and the rate is ' + colors.green(config.rate) + ' Hz.');
-    var log = ' This musics will be played ';
-    for (var zik in list) {
-        log += '\n- '
-        log += list[zik].name;
+  var pkg = require('./package.json');
+  require('check-update-github')({
+    name: pkg.name,
+    currentVersion: pkg.version,
+    user: 'cedced19'
+  }, function (err, lastestVersion, defaultMessage) {
+    if (!err) {
+      console.log(colors.cyan('[' + time() + '] ') + defaultMessage);
     }
-    console.log(colors.cyan('['+ time() + ']') + log)
-    random();
-}
+  });
+  console.log(colors.cyan('[' + time() + ']') + ' Now this will broadcast on ' + colors.green(config.freq) + ' MHz and the rate is ' + colors.green(config.rate) + ' Hz.');
+  var log = ' This musics will be played ';
+  for (var zik in list) {
+    log += '\n- ';
+    log += list[zik].name;
+  }
+  console.log(colors.cyan('[' + time() + ']') + log);
+  random();
+};
 
 start();
